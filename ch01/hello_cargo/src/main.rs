f6f3142606a4496b20037322c4b0a2a8c50bb282
jefedeltodos <jefedeltodos@outlook.com>
2024-11-14 01:04:20 +0000

    feat: added hello world program using cargo

---
+++ ch01/hello_cargo/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// `cargo build` to build
+// `cargo run` to build and run the executable
+// `cargo check` to check that the project will build, but does not output an executable.
+// `cargo build --release` to build for production (longer compile) 
