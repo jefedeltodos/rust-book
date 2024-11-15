584513dfbd3c9af3f4dc4b3a6e6596e41ac8ecb0
jefedeltodos <jefedeltodos@outlook.com>
2024-11-15 03:16:10 +0000

    feat: removed showing the secret number

--- ch02/guessing_game/src/main.rs
+++ ch02/guessing_game/src/main.rs
@@ -2,20 +2,24 @@ use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
-    println!("The secret number is {secret_number}.");
+    // println!("The secret number is {secret_number}.");
     
     loop {
+        println!("Guess the number!");
+        println!("Please input your guess");
         let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
